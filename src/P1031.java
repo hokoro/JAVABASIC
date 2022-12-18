f02cbef5e39fe757547cc2c5ec32ba56763f01d4
영성천 <hokoro123@gmail.com>
2022-12-18 17:54:05 +0900

    JAVA BASIC 10

--- src/P1031.java
+++ src/P1031.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class P1031 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.printf("%o",n);
+    }
 }
